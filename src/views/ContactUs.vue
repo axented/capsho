2cf3e22b6682f3362775c4fbaed83994f9d27ffb
Rafael Serna <rafael.serna@axented.com>
2021-08-12 14:34:07 -0500

    Created Contact page

---
+++ src/views/ContactUs.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="bg-backgroundGradient py-10 px-10">
+    <!-- Body -->
+    <div class="flex flex-col rounded-xl bg-white py-10 px-10">
+      <span class="font-heading text-3xl text-left">We love hearing from you, contact us!</span>
+      <form class="flex flex-col py-6">
+        <div class="py-2">
+          <label class="block text-left text-formLabel font-formText pb-2" for="fullName">Full Name</label>
+          <input class="apperance-none bg-backgroundGradient rounded-xl w-full py-4 px-4 text-primaryDark focus:outline-none focus:shadow-outline" id="fullName" type="text" placeholder="First and last name">
+        </div>
+        <div class="py-2">
+          <label class="block text-left text-formLabel font-formText pb-2" for="email">Email</label>
+          <input class="apperance-none bg-backgroundGradient rounded-xl w-full py-4 px-4 text-primaryDark focus:outline-none focus:shadow-outline" id="email" type="email" placeholder="user@example.com">
+        </div>
+        <div class="py-2">
+          <label class="block text-left text-formLabel font-formText pb-2" for="comment">Comment</label>
+          <textarea class="apperance-none bg-backgroundGradient rounded-xl resize-none w-full py-4 px-4 text-primaryDark focus:outline-none focus:shadow-outline" rows="5" id="comment" placeholder="Would love to hear from you!"></textarea>
+        </div>
+        <div class="flex py-2 justify-start">
+          <button class="self-end">
+            <span class="text-left font-body font-bold text-primaryDark">Upload brand logotype</span>
+          </button>
+        </div>
+        <div class="flex py-4 justify-end">
+          <button class="bg-primaryDark rounded-full py-4 px-6">
+            <span class="text-white font-body font">Continue</span>
+          </button>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Contact Us'
+}
+</script>
