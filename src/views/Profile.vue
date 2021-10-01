d15c41e8d8332364ca85478cc7b730c56aaa38fe
Rafael Serna <rafael.serna@axented.com>
2021-10-01 16:53:33 -0500

    Switch manage links

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -71,14 +71,14 @@
             <span class="font-heading text-3xl text-left pb-2">Account Info</span>
             <div class="flex flex-col py-4">
               <div class="flex justify-start">
-                <a :href="`https://payments.stunning.co/manage_subscription/3438jgbacfixamwxmcqpdyzbi/${user.stripeId}`" target="_blank">
+                <a :href="`https://payments.stunning.co/payment_update/3438jgbacfixamwxmcqpdyzbi/${user.stripeId}`" target="_blank">
                   <button class="text-left font-body font-bold text-primaryDark">
                     Manage billing info
                   </button>
                 </a>
               </div>
               <div class="flex justify-start">
-                <a :href="`https://payments.stunning.co/payment_update/3438jgbacfixamwxmcqpdyzbi/${user.stripeId}`" target="_blank">
+                <a :href="`https://payments.stunning.co/manage_subscription/3438jgbacfixamwxmcqpdyzbi/${user.stripeId}`" target="_blank">
                   <button class="text-left font-body font-bold text-primaryDark pt-4">
                     Manage plan
                   </button>
